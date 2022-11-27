c7bf834c5049c1b32f303e82001c5846f00424db
gilbertosantana <gilbertosantoss307@gmail.com>
2022-11-26 21:57:20 -0300

    Exercício utilizando HashSet

---
+++ SetExercice/Entities/LogRecord.cs
@@ -0,0 +1,29 @@
+﻿namespace SetExercice.Entities
+{
+    internal class LogRecord
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        public LogRecord(string name, DateTime date)
+        {
+            Name = name;
+            Date = date;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord? other = obj as LogRecord;
+            return Name.Equals(other!.Name);
+        }
+    }
+}
---
+++ SetExercice/Program.cs
@@ -0,0 +1,27 @@
+﻿using SetExercice.Entities;
+
+HashSet<LogRecord> set = new HashSet<LogRecord>(); ;
+
+Console.Write("Enter file full path: ");
+
+string path = @Console.ReadLine()!;
+#if !DEFAULT
+try
+{
+    using(StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine()!.Split(' ');
+            string name = line[0];
+            DateTime instant = DateTime.Parse(line[1]);
+            set.Add(new LogRecord(name, instant));
+        }
+        Console.WriteLine("Total users: " + set.Count);
+    }
+}
+catch(IOException e)
+{
+    Console.WriteLine("An error ocurred");
+	Console.WriteLine(e.Message);
+}
